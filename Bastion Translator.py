a7ff579f2cb66673350c069f417fc91ba2dd9eee
Gimich <gabriel.luapin@hotmail.fr>
2018-11-11 01:20:55 +0100

    Add files via upload

---
+++ Bastion Translator.py
@@ -0,0 +1,45 @@
+# Work with Python 3.6
+import discord
+import os
+
+client = discord.Client()
+
+TOKEN = os.environ["TOKEN"]
+
+gimich = os.environ["Gimich"]
+
+@client.event
+async def on_message(message):
+	# we do not want the bot to reply to itself
+	messageRecue = message.content.lower().split(" ")
+	if message.author == client.user:
+		return
+
+	if "dweeee" in messageRecue and message.author.id == gimich:
+		msg = 'Bastion a dit : \"Bonjour\"'.format(message)
+		await client.send_message(message.channel, msg)
+
+	if "dwuuuuuuu" in messageRecue and message.author.id == gimich:
+		msg = 'Bastion est triste'.format(message)
+		await client.send_message(message.channel, msg)
+
+	if "dwuiii" in messageRecue and message.author.id == gimich:
+		msg = 'Bastion a dit : \"Merci\"'.format(message)
+		await client.send_message(message.channel, msg)
+
+	if "dwui" in messageRecue and message.author.id == gimich:
+		msg = 'Bastion a dit : \"Oui\"'.format(message)
+		await client.send_message(message.channel, msg)
+
+	if "dwuiiiiiii" in messageRecue and message.author.id == gimich:
+		msg = 'Bastion a dit : \"Merci\"'.format(message)
+		await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+	print('Logged in as')
+	print(client.user.name)
+	print(client.user.id)
+	print('------')
+
+client.run(TOKEN)
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python "Bastion Translator.py"
