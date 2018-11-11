ac0d3ed132742bb2626455446d29cc4f1954303c
Gimich <gabriel.luapin@hotmail.fr>
2018-11-11 13:51:16 +0100

    Update Bastion Translator.py

--- Bastion Translator.py
+++ Bastion Translator.py
@@ -8,7 +8,7 @@ TOKEN = os.environ["TOKEN"]
 
 gimich = os.environ["Gimich"]
 
-bastionEmoji = "Bastion:" + os.environ["Bastion"] 
+discord.emoji("Bastion" + os.environ["Bastion"])
 
 @client.event
 async def on_message(message):
