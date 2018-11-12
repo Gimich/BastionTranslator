06e5aba78d85a09f9853fbd4b46e248fab908357
Gimich <gabriel.luapin@hotmail.fr>
2018-11-12 13:15:05 +0100

    Update Bastion Translator.py

--- Bastion Translator.py
+++ Bastion Translator.py
@@ -12,29 +12,30 @@ bastionEmoji = "<:Bastion:" + os.environ["Bastion"] + ">"
 
 @client.event
 async def on_message(message):
-	# we do not want the bot to reply to itself
+	auteur = message.author
+	
 	messageRecue = message.content.lower().split(" ")
 	if message.author == client.user:
 		return
 
 	if "dweeee" in messageRecue:
-		msg = (message.author(username) + " : " + '  a dit : \"Bonjour\"'.format(message))
+		msg = (auteur.username() + " : " + '  a dit : \"Bonjour\"'.format(message))
 		await client.send_message(message.channel, msg)
 
 	if "dwuuuuuuu" in messageRecue:
-		msg = (message.author(username) + " : " + ' est triste'.format(message))
+		msg = (auteur.username() + " : " + ' est triste'.format(message))
 		await client.send_message(message.channel, msg)
 
 	if "dwuiii" in messageRecue:
-		msg = (message.author(username) + " : " + ' a dit : \"Merci\"'.format(message))
+		msg = (auteur.username() + " : " + ' a dit : \"Merci\"'.format(message))
 		await client.send_message(message.channel, msg)
 
 	if "dwui" in messageRecue:
-		msg = (message.author(username) + " : " + ' a dit : \"Oui\"'.format(message))
+		msg = (auteur.username() + " : " + ' a dit : \"Oui\"'.format(message))
 		await client.send_message(message.channel, msg)
 
 	if "dwuiiiiiii" in messageRecue:
-		msg = (message.author(username) + " : " + ' est content'.format(message))
+		msg = (auteur.username() + " : " + ' est content'.format(message))
 		await client.send_message(message.channel, msg)
 
 @client.event
