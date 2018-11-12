b07e3be6707116b607ea4fe2383bd189888120e1
Gimich <gabriel.luapin@hotmail.fr>
2018-11-12 13:42:15 +0100

    Update Bastion Translator.py

--- Bastion Translator.py
+++ Bastion Translator.py
@@ -18,7 +18,7 @@ async def on_message(message):
 		return
 
 	if "dweeee" in messageRecue:
-		msg = (message.author.nick + " : " + 'a dit : \"Bonjour\"'.format(message))
+		msg = (message.author.nick + 'a dit : \"Bonjour\"'.format(message))
 		await client.send_message(message.channel, msg)
 
 	if "dwuuuuuuu" in messageRecue:
@@ -26,11 +26,11 @@ async def on_message(message):
 		await client.send_message(message.channel, msg)
 
 	if "dwuiii" in messageRecue:
-		msg = (message.author.nick + " : " + 'a dit : \"Merci\"'.format(message))
+		msg = (message.author.nick + 'a dit : \"Merci\"'.format(message))
 		await client.send_message(message.channel, msg)
 
 	if "dwui" in messageRecue:
-		msg = (message.author.nick + " : " + 'a dit : \"Oui\"'.format(message))
+		msg = (message.author.nick + 'a dit : \"Oui\"'.format(message))
 		await client.send_message(message.channel, msg)
 
 	if "dwuiiiiiii" in messageRecue:
