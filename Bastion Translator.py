086f9dd6ae35a418becd4b136cbaf9a3213232c5
Gimich <gabriel.luapin@hotmail.fr>
2018-11-20 15:42:20 +0100

    Update Bastion Translator.py

--- Bastion Translator.py
+++ Bastion Translator.py
@@ -24,7 +24,7 @@ async def on_message(message):
 		return
 
 	if "dweeee" in messageRecue:
-		if (message.timestamp.time().hour + 1) % 24 < 18 and (message.timestamp.time().hour + 1) %24 > 6:
+		if (message.timestamp.time().hour + 1) % 24 < 18 and (message.timestamp.time().hour + 1) %24 > 4:
 			msg = (auteur + ' dit : \"Bonjour\"'.format(message))
 		else : msg = (auteur + ' dit : \"Bonsoir\"'.format(message))
 		await client.send_message(message.channel, msg)
